a8f04b956d72fc32ac0f4a3fc9ca73e73240cc25
Florian Knödl <florian@sirpauls.com>
2019-12-18 10:19:51 +0100

    align stuff centered

--- sketch.js
+++ sketch.js
@@ -1,14 +1,36 @@
 /* code of the game */
 
+var posX, posY;
+var cursorW, cursorH;
+
 function setup(){
-  createCanvas(640, 480);
-  fill(255);
-  noStroke();
+    createCanvas(640, 480);
+    fill(255);
+    noStroke();
+
+    cursorW = 50;
+    cursorH = 5;
+    posX = width/2 - cursorW/2;
+    posY = height - 20;
 }
 
 
 function draw() {
-  clear();
-  background(255, 0, 100);
-  rect(30, 20, 55, 55);
+
+    /* calculate math things first */
+    calculateStuff();
+
+    /* display what you calculated */
+    displayStuff();
+}
+
+function calculateStuff(){
+
+}
+
+
+function displayStuff(){
+    clear();
+    background(255, 0, 100);
+    rect(posX, posY, cursorW, cursorH);
 }
